c7997665b68559ff50dfd665c451a30ef4077c85
Cha-Q <quentin.chassaing@outlook.com>
2022-01-26 14:03:52 +0100

    Update categorie_model

--- Models/categorie_model.php
+++ Models/categorie_model.php
@@ -1,5 +1,44 @@
 <?php
     class Categorie{
-        
+        // attributs
+        private $id;
+        private $nomCom;
+        private $content;
+        private $dateCom;
+
+        // constructeur
+        public function __construct($id,$nomCom,$content,$dateCom){
+            $this->id = $id;
+            $this->nomCom = $nomCom;
+            $this->content = $content;
+            $this->dateCom = $dateCom;
+        }
+
+        // getters
+        public function getIdCom(){
+            return $this->id;
+        }
+        public function getNomRole(){
+            return $this->nomCom;
+        }
+        public function getContent(){
+            return $this->content;
+        } 
+        public function getDateCom(){
+            return $this->dateCom;
+        }
+        // setters
+        public function setIdCom($newid){
+            $this->id = $newid;
+        }
+        public function setNomCom($newNomCom){
+            $this->nomCom = $newNomCom;
+        }
+        public function setContent($newContent){
+            $this->content = $newContent;
+        }
+        public function setDateCom($newDateCom){
+            $this->dateCom = $newDateCom;
+        }
     }
 ?>
