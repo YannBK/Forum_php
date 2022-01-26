eab7d0e57b2f2ef3b701122bb3298ae7ed92677d
Cha-Q <quentin.chassaing@outlook.com>
2022-01-26 15:40:00 +0100

    Update creer_artcile

--- Views/creerArticle_view.php
+++ Views/creerArticle_view.php
@@ -1,9 +1,11 @@
 <h2>Rédigez votre sujet</h2>
-
+<a href="index.php?p=home">Accueil</a>
 <form action="" method="POST">
     <label for="nom-sujet">Nom du sujet</label>
     <input type="text" name="nom-sujet" id="nom-sujet" min="2" max="50">
     <label for="contenu-sujet">votre magnifique texte ici :</label>
-    <textarea name="contenu-sujet" id="contenu-sujet" cols="50" rows="30"></textarea>
+    <div>
+        <textarea name="contenu-sujet" id="contenu-sujet" cols="50" rows="30" style="margin-top:10px;"></textarea>
+    </div>
+    
 </form>
-<a href="index.php?p=home">Accueil</a>
