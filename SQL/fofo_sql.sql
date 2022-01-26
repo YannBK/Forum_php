ce07f31b853262497a61f0b25250885097d014b8
Cha-Q <quentin.chassaing@outlook.com>
2022-01-26 10:11:19 +0100

    Create fofo_sql.sql

---
+++ SQL/fofo_sql.sql
@@ -0,0 +1,62 @@
+create database forum;
+use forum;
+
+create table users (
+	id_users int not null auto_increment primary Key,
+    login_user varchar(50),
+    mdp_user varchar(255)
+);
+
+create table role (
+	id_role int not null auto_increment primary Key,
+    nom_role varchar(50)
+);
+
+create table sujet(
+	id_sujet int not null auto_increment primary Key,
+    nom_sujet varchar(50),
+    contenu_sujet text,
+    date_sujet date
+);
+
+create table commentaire(
+	id_commentaire int not null auto_increment primary Key,
+    nom_com varchar(50),
+    contenu_com text,
+    date_com date
+);
+
+create table categorie(
+	id_categorie int not null auto_increment primary Key,
+    nom_cat varchar(50)
+    );
+    
+create table appartenir(
+	id_sujet int,
+    id_categorie int,
+    primary key(id_sujet,id_categorie),
+    constraint fk_appartenir_sujet foreign key (id_sujet) references sujet(id_sujet),
+    constraint fk_appartenir_categorie foreign key (id_categorie) references categorie(id_categorie)
+    );
+    
+    
+Alter table users
+add id_role int,
+add constraint fk_users_role foreign key (id_role) references role(id_role);
+
+Alter table sujet
+add id_users int,
+add constraint fk_sujet_users foreign key (id_users) references users(id_users);
+
+Alter table commentaire
+add id_users int,
+add id_sujet int,
+add constraint fk_commentaire_users foreign key (id_users) references users(id_users),
+add constraint fk_commentaire_sujet foreign key (id_sujet) references sujet(id_sujet);
+
+
+
+
+
+
+
