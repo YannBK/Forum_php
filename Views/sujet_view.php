563e518cde815b330957e0a9ebb28d8bfa38def9
SebLeyr <sebastien.leyrissoux@gmail.com>
2022-01-27 15:16:17 +0100

    avance model sujet

--- Views/sujet_view.php
+++ Views/sujet_view.php
@@ -1,15 +1,9 @@
 <h1>page sujet</h1>
 <a href="index.php?p=home">Accueil</a>
 
-"<div>
-    <h2>nom sujet</h2>
-    <p>auteur</p>
-    <p>date crea</p>
-    <p>contenu</p>
-</div>"
-"<div>
-    <h2>Commentaires : </h2>
-    <p>auteur</p>
-    <p>date creation</p>
-    <p>contenu</p>
-</div>"
+<div>
+    <?= $sujet ?>
+</div>
+<div>
+    <?= $listeCom ?>
+</div>
