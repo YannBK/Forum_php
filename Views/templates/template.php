188b3d94bcf4091d663c3b23642343bede2d730a
Cha-Q <quentin.chassaing@outlook.com>
2022-01-31 09:16:17 +0100

    correction du champ email

--- Views/templates/template.php
+++ Views/templates/template.php
@@ -54,7 +54,8 @@
                         </li>
                         <li>
                             <label for="email">E-mail </label>
-                            <input type="email" name="email-crea" required pattern="^[A-Za-z]+@{1}[A-Za-z]+\.{1}[A-Za-z]{2,}$"/>
+                            <input type="email" name="email-crea" required pattern="(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])
+"/>
                         </li>
                         <li>
                             <label for="dateN">Date de naissance </label>
